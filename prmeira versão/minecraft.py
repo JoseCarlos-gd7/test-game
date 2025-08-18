802a917723acbd00736af78c36d61f70413f0399
JoseCarlos-gd7 <josewallpappers@gmail.com>
2025-08-18 14:18:28 -0300

    Add files via upload

---
+++ prmeira versão/minecraft.py
@@ -0,0 +1,267 @@
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.gui.OnscreenImage import OnscreenImage
+from panda3d.core import loadPrcFile
+from panda3d.core import DirectionalLight, AmbientLight
+from panda3d.core import TransparencyAttrib
+from panda3d.core import WindowProperties
+from panda3d.core import ClockObject
+from panda3d.core import CollisionTraverser, CollisionNode
+from panda3d.core import CollisionBox, CollisionRay, CollisionHandlerQueue
+
+loadPrcFile('settings.prc')
+globalClock = ClockObject.getGlobalClock()
+
+def degToRad(degrees):
+    return degrees * (pi / 180.0)
+
+class Minecraft(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.play_music("theme.ogg")
+
+        self.selectedBlockType = 'sand'
+
+        self.loadModels()
+        self.setupLights()
+        self.generateTerrain()
+        self.setupCamera()
+        self.setupSkybox()
+        self.captureMouse()
+        self.setupControls()
+
+        self.taskMgr.add(self.update, 'update')
+    
+    def update(self, task):
+        dt = globalClock.getDt()
+
+        playerMoveSpeed = 10
+        x_movement = 0
+        y_movement = 0
+        z_movement = 0
+
+        if self.keyMap['forward']:
+            x_movement -= dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+            y_movement += dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+        if self.keyMap['backward']:
+            x_movement += dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+            y_movement -= dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+        if self.keyMap['left']:
+            x_movement -= dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+            y_movement -= dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+        if self.keyMap['right']:
+            x_movement += dt * playerMoveSpeed * cos(degToRad(self.camera.getH()))
+            y_movement += dt * playerMoveSpeed * sin(degToRad(self.camera.getH()))
+        if self.keyMap['up']:
+            z_movement += dt * playerMoveSpeed
+        if self.keyMap['down']:
+            z_movement -= dt * playerMoveSpeed
+        
+        self.camera.setPos(
+            self.camera.getX() + x_movement,
+            self.camera.getY() + y_movement,
+            self.camera.getZ() + z_movement,
+        )
+
+        if self.cameraSwingActivated:
+            md = self.win.getPointer(0)
+            mouseX = md.getX()
+            mouseY = md.getY()
+
+            mouseChangeX = mouseX - self.lastMouseX
+            mouseChangeY = mouseY - self.lastMouseY
+
+            self.cameraSwingFactor = 50
+
+            currentH = self.camera.getH()
+            currentP = self.camera.getP()
+
+            self.camera.setHpr(
+                currentH - mouseChangeX * dt * self.cameraSwingFactor,
+                min(90, max(-90, currentP - mouseChangeY * dt * self.cameraSwingFactor)),
+                0
+            )
+
+            self.lastMouseX = mouseX
+            self.lastMouseY = mouseY
+
+        return task.cont
+    
+    def updateKeyMap(self, key, value):
+        self.keyMap[key] = value
+    
+    def SelectedBlockType(self, type):
+        self.selectedBlockType = type
+    
+    def captureMouse(self):
+        self.cameraSwingActivated = True
+
+        md = self.win.getPointer(0)
+        self.lastMouseX = md.getX()
+        self.lastMouseY = md.getY()
+
+        properties = WindowProperties()
+        properties.setCursorHidden(True)
+        properties.setMouseMode(WindowProperties.M_relative)
+        self.win.requestProperties(properties)
+    
+    def releaseMouse(self):
+        self.cameraSwingActivated = False
+
+        properties = WindowProperties()
+        properties.setCursorHidden(False)
+        properties.setMouseMode(WindowProperties.M_absolute)
+        self.win.requestProperties(properties)
+    
+    def leftClick(self):
+        self.captureMouse()
+        self.removeBlock()
+    
+    def loadModels(self):
+        self.dirtBlock = self.loader.loadModel('dirt-block.glb')
+        self.stoneBlock = self.loader.loadModel('stone-block.glb')
+        self.sandBlock = self.loader.loadModel('sand-block.glb')
+    
+    def removeBlock(self):
+        if self.rayQueue.getNumEntries() > 0:
+            self.rayQueue.sortEntries()
+            rayHit = self.rayQueue.getEntry(0)
+
+            hitNodePath = rayHit.getIntoNodePath()
+            hitObject = hitNodePath.getPythonTag('owner')
+            distanceFromPlayer = hitObject.getDistance(self.camera)
+
+            if distanceFromPlayer < 12:
+                hitNodePath.clearPythonTag('owner')
+                hitObject.removeNode()
+                self.play_sound("remove_block.ogg") 
+    
+    def createNewBlock(self, x, y, z, type):
+        newBlockNode = self.render.attachNewNode('new-block-placeholder')
+        newBlockNode.setPos(x, y, z)
+
+        if type == 'dirt':
+            self.dirtBlock.instanceTo(newBlockNode)
+        elif type == 'sand':
+            self.sandBlock.instanceTo(newBlockNode)
+        elif type == 'stone':
+            self.stoneBlock.instanceTo(newBlockNode)
+        
+        blockSolid = CollisionBox((-1, -1, -1), (1, 1, 1))
+        blockNode = CollisionNode('block-collision-node')
+        blockNode.addSolid(blockSolid)
+        collider = newBlockNode.attachNewNode(blockNode)
+        collider.setPythonTag('owner', newBlockNode)
+    
+    def placeBlock(self):
+        if self.rayQueue.getNumEntries() > 0:
+            self.rayQueue.sortEntries()
+            rayHit = self.rayQueue.getEntry(0)
+            hitNodePath = rayHit.getIntoNodePath()
+            normal = rayHit.getSurfaceNormal(hitNodePath)
+            hitObject = hitNodePath.getPythonTag('owner')
+            distanceFromPlayer = hitObject.getDistance(self.camera)
+
+            if distanceFromPlayer < 14:
+                hitBlockPos = hitObject.getPos()
+                newBlockPos = hitBlockPos + normal * 2
+                self.createNewBlock(newBlockPos.x, newBlockPos.y, newBlockPos.z, self.selectedBlockType)
+                self.play_sound("create_block.ogg")
+    
+    def setupControls(self):
+        self.keyMap = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False,
+            "up": False,
+            "down": False,
+        }
+
+        self.accept('escape', self.releaseMouse)
+        self.accept('mouse1', self.leftClick)
+        self.accept('mouse3', self.placeBlock)
+
+        self.accept('w', self.updateKeyMap, ['forward', True])
+        self.accept('w-up', self.updateKeyMap, ['forward', False])
+        self.accept('a', self.updateKeyMap, ['left', True])
+        self.accept('a-up', self.updateKeyMap, ['left', False])
+        self.accept('s', self.updateKeyMap, ['backward', True])
+        self.accept('s-up', self.updateKeyMap, ['backward', False])
+        self.accept('d', self.updateKeyMap, ['right', True])
+        self.accept('d-up', self.updateKeyMap, ['right', False])
+        self.accept('space', self.updateKeyMap, ['up', True])
+        self.accept('space-up', self.updateKeyMap, ['up', False])
+        self.accept('lshift', self.updateKeyMap, ['down', True])
+        self.accept('lshift-up', self.updateKeyMap, ['down', False])
+
+        self.accept('1', self.SelectedBlockType, ['dirt'])
+        self.accept('2', self.SelectedBlockType, ['sand'])
+        self.accept('3', self.SelectedBlockType, ['stone'])
+    
+    def setupCamera(self):
+        self.disableMouse()
+        self.camera.setPos(0, 0, 3)
+        self.camLens.setFov(80)
+
+        crosshairs = OnscreenImage(
+            image = 'crosshairs.png',
+            pos = (0, 0, 0),
+            scale = 0.05,
+        )
+        crosshairs.setTransparency(TransparencyAttrib.MAlpha)
+
+        self.cTrav = CollisionTraverser()
+        ray = CollisionRay()
+        ray.setFromLens(self.camNode, (0, 0))
+        rayNode = CollisionNode('line-of-sight')
+        rayNode.addSolid(ray)
+        rayNodePath = self.camera.attachNewNode(rayNode)
+        self.rayQueue = CollisionHandlerQueue()
+        self.cTrav.addCollider(rayNodePath, self.rayQueue)
+    
+    def setupSkybox(self):
+        skybox = self.loader.loadModel('skybox/skybox.egg')
+        skybox.setScale(500)
+        skybox.setBin('background', 1)
+        skybox.setDepthWrite(0)
+        skybox.setLightOff()
+        skybox.reparentTo(self.render)
+    
+    def generateTerrain(self):
+        for z in range(10):
+            for y in range(20):
+                for x in range(20):
+                    self.createNewBlock(
+                        x * 2 - 20,
+                        y * 2 - 20,
+                        -z * 2,
+                        'sand' if z == 0 else 'dirt'
+                    )
+    
+    def setupLights(self):
+        mainLight = DirectionalLight('main light')
+        mainLightNodePath = self.render.attachNewNode(mainLight)
+        mainLightNodePath.setHpr(30, -60, 0)
+        self.render.setLight(mainLightNodePath)
+
+        ambientLight = AmbientLight('ambient light')
+        ambientLight.setColor((0.3, 0.3, 0.3, 1))
+        ambientLightNodePath = self.render.attachNewNode(ambientLight)
+        self.render.setLight(ambientLightNodePath)
+    
+    def play_music(self, file_name, loop=True, volume=0.3):
+        self.music = self.loader.loadMusic(file_name)
+        self.music.setLoop(loop)
+        self.music.setVolume(volume)
+        self.music.play()
+    
+    def play_sound(self, file_name, volume=1.0):
+        sound = self.loader.loadSfx(file_name)
+        sound.setVolume(volume)
+        sound.play()
+
+game = Minecraft()
+game.run()
